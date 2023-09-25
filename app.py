c1335a1f6d99c15aa50bab7d2336b7006a456f7e
noumanirshad <noumanirshad564@gmail.com>
2023-09-25 06:02:50 -0700

    shorten directory

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from src.Hate_Speech_Classifier.logger import logging
 from src.Hate_Speech_Classifier.exception import CustomException
 import sys
-# from src.Hate_Speech_Classifier.components.data_ingestion import DataIngestion
+from src.Hate_Speech_Classifier.components.data_ingestion import DataIngestion
 # from src.Hate_Speech_Classifier.components.data_transformation import DataTransformation
-# import logging
+
 import pandas as pd
 # from src.Hate_Speech_Classifier.utils import read_sql_data
-
-from sklearn.model_selection import train_test_split
 
 
 if __name__ == '__main__':
@@ -15,9 +13,9 @@ if __name__ == '__main__':
 
 
     try:
-        # data_ingestion = DataIngestion()
-        # train_path, test_path = data_ingestion.initiate_data_ingestion()
-        1/0
+        data_ingestion = DataIngestion()
+        train_path, test_path = data_ingestion.get_data_ingestion()
+
         # data_transformation = DataTransformation()
         # data_transformation.initiate_data_transformation(train_path, test_path)
 
